feabb73415e1a6de4aaf5e28c1b781ff4e6bf4f0
MilosEcon <mikizalo30@gmail.com>
2022-04-08 04:27:50 +0200

    a

--- js/model.js
+++ js/model.js
@@ -68,7 +68,7 @@ const model = {
             id: 2,
             image: 'https://images.unsplash.com/photo-1588556008426-af415581d44b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=930&q=80',
             title: 'Halv Pris På Taco!',
-            description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quasi iure tempora error. Fugit, iusto aliquam nulla nostrum sequi possimus quaerat culpa exercitationem ad eos aspernatur quae corporis ratione magni velit quibusdam minus quam consequatur voluptatibus quis. Recusandae quos voluptate nobis voluptatibus cum praesentium laborum exercitationem deserunt dolor unde et eaque cumque quia id provident, officiis quisquam aperiam, nisi aut. Tempore, hic. Ipsum eveniet sint odit, cupiditate, beatae, ducimus ad blanditiis aliquam dignissimos molestiae natus quis necessitatibus culpa quibusdam voluptate quisquam?',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugit nisi similique, veritatis dolor impedit. Consectetur, sit! Neque accusamus deleniti, eos perferendis veniam incidunt corrupti eaque aut qui. Eveniet quidem enim commodi ipsum aspernatur nam quo delectus eum corporis quaerat, animi, rerum recusandae reprehenderit natus ad, deserunt facilis. Velit, veniam fuga ipsa repellat eos asperiores maxime voluptatem quam porro harum nostrum explicabo tempora animi reiciendis.',
             category: {
                 id: 3,
                 icon: 'restaurant.svg',
@@ -89,7 +89,7 @@ const model = {
             id: 3,
             image: 'https://images.unsplash.com/photo-1470229722913-7c0e2dbbafd3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
             title: 'Alexander Rybak Konsert',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus illo et nisi iure! Unde nesciunt obcaecati quidem porro itaque impedit cumque, minus quos sed exercitationem ipsam aut blanditiis eos nostrum incidunt voluptatibus sint numquam beatae sunt est dicta expedita veniam! Voluptatum magni fugit maiores eum vero molestiae commodi impedit voluptate quam perferendis. Mollitia repellendus quasi assumenda quidem reprehenderit qui incidunt quibusdam, illo minus tempora aperiam ducimus nam velit voluptatibus, veritatis placeat nihil, error nemo. Veniam optio minus dolore eligendi?',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugit nisi similique, veritatis dolor impedit. Consectetur, sit! Neque accusamus deleniti, eos perferendis veniam incidunt corrupti eaque aut qui. Eveniet quidem enim commodi ipsum aspernatur nam quo delectus eum corporis quaerat, animi, rerum recusandae reprehenderit natus ad, deserunt facilis. Velit, veniam fuga ipsa repellat eos asperiores maxime voluptatem quam porro harum nostrum explicabo tempora animi reiciendis.',
             category: {
                 id: 1,
                 icon: 'music.svg',
@@ -110,7 +110,7 @@ const model = {
             id: 4,
             image: 'https://images.unsplash.com/photo-1631779202803-7f1415c87a9d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
             title: 'Skiskole For Barn',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus illo et nisi iure! Unde nesciunt obcaecati quidem porro itaque impedit cumque, minus quos sed exercitationem ipsam aut blanditiis eos nostrum incidunt voluptatibus sint numquam beatae sunt est dicta expedita veniam! Voluptatum magni fugit maiores eum vero molestiae commodi impedit voluptate quam perferendis. Mollitia repellendus quasi assumenda quidem reprehenderit qui incidunt quibusdam, illo minus tempora aperiam ducimus nam velit voluptatibus, veritatis placeat nihil, error nemo. Veniam optio minus dolore eligendi?',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugit nisi similique, veritatis dolor impedit. Consectetur, sit! Neque accusamus deleniti, eos perferendis veniam incidunt corrupti eaque aut qui. Eveniet quidem enim commodi ipsum aspernatur nam quo delectus eum corporis quaerat, animi, rerum recusandae reprehenderit natus ad, deserunt facilis. Velit, veniam fuga ipsa repellat eos asperiores maxime voluptatem quam porro harum nostrum explicabo tempora animi reiciendis.',
             category: {
                 id: 4,
                 icon: 'course.svg',
@@ -131,7 +131,7 @@ const model = {
             id: 5,
             image: 'https://images.unsplash.com/photo-1587825140708-dfaf72ae4b04?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80',
             title: 'Statsministeren Holder Tale',
-            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci possimus illo et nisi iure! Unde nesciunt obcaecati quidem porro itaque impedit cumque, minus quos sed exercitationem ipsam aut blanditiis eos nostrum incidunt voluptatibus sint numquam beatae sunt est dicta expedita veniam! Voluptatum magni fugit maiores eum vero molestiae commodi impedit voluptate quam perferendis. Mollitia repellendus quasi assumenda quidem reprehenderit qui incidunt quibusdam, illo minus tempora aperiam ducimus nam velit voluptatibus, veritatis placeat nihil, error nemo. Veniam optio minus dolore elig',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit fugit nisi similique, veritatis dolor impedit. Consectetur, sit! Neque accusamus deleniti, eos perferendis veniam incidunt corrupti eaque aut qui. Eveniet quidem enim commodi ipsum aspernatur nam quo delectus eum corporis quaerat, animi, rerum recusandae reprehenderit natus ad, deserunt facilis. Velit, veniam fuga ipsa repellat eos asperiores maxime voluptatem quam porro harum nostrum explicabo tempora animi reiciendis.',
             category: {
                 id: 2,
                 icon: 'happening.svg',
